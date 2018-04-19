6c8656cd6c0f6b6575db331731646e490d5d4cb5
Areaxe <2860349275@qq.com>
2018-04-19 23:10:13 +0800

    add ball

---
+++ ball/index.js
@@ -0,0 +1,51 @@
+let canvas = null;
+let ballList = [];
+let cxt;
+window.onload = function () {
+  canvas = document.querySelector('#canvas');
+  canvas.width = '600';
+  canvas.height = '400';
+  cxt = canvas.getContext('2d');
+  canvas.addEventListener('mousemove', (event) => {
+    new Ball(event.clientX, event.clientY);
+  });
+  for(let i=0;i<5;i++){
+    new Ball(100,100);
+  }
+  setInterval(()=>{
+    cxt.clearRect(0,0,canvas.width,canvas.height);
+    for(let i=0;i<ballList.length;i++){
+      ballList[i].update();
+      if(ballList[i].r>0)
+        ballList[i].render();
+    }
+  },20);
+}
+
+let Ball = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.r = 30;
+  this.dx = Math.random() * 10 - 5;
+  this.dy = Math.random() * 10 - 5;
+  this.dr = Math.random() + 0.3;
+  ballList.push(this);
+}
+
+Ball.prototype = {
+  update: function () {
+  this.x += this.dx;
+  this.y += this.dy;
+  this.r -= this.dr;
+  if(this.r <=0){
+    ballList = _.without(ballList,this);
+  }
+  },
+  render: function () {
+    cxt.beginPath();
+    cxt.arc(this.x,this.y,this.r,0,Math.PI*2,true);
+    cxt.fillStyle = 'blue';
+    cxt.fill();
+    cxt.closePath();
+  }
+}
